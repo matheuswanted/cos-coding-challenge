de9393a27d66af0c450b3b75647389595e1e6ff7
Matheus Santos <matheuswantedac@live.com>
2021-12-09 08:04:58 -0300

    Implement test for AuctionStatiscsAggregate to ensure all metrics are calculated correctly

--- challenges/backend/src/app/domain/AuctionStatisticsAggregate/classes/AuctionStatisticsAggregate.ts
+++ challenges/backend/src/app/domain/AuctionStatisticsAggregate/classes/AuctionStatisticsAggregate.ts
@@ -1,6 +1,15 @@
 import { IAuction, IAuctionStatisticsAggregate } from "../interface/IAuctionStatisticsAggregate";
 
 export class AuctionStatisticsAggregate implements IAuctionStatisticsAggregate {
+    public getAverageProgress(): number {
+        throw new Error("Method not implemented.");
+    }
+    public getNumberOfAuctions(): number {
+        throw new Error("Method not implemented.");
+    }
+    public getAverageNumberOfBids(): number {
+        throw new Error("Method not implemented.");
+    }
     public addAuction(auction: IAuction): void {
         throw new Error("Method not implemented.");
     }
--- challenges/backend/src/app/domain/AuctionStatisticsAggregate/interface/IAuctionStatisticsAggregate.ts
+++ challenges/backend/src/app/domain/AuctionStatisticsAggregate/interface/IAuctionStatisticsAggregate.ts
@@ -1,11 +1,15 @@
 
 export interface IAuctionStatisticsAggregate {
+    getAverageProgress(): number;
+    getNumberOfAuctions(): number;
+    getAverageNumberOfBids(): number;
     addAuction(auction: IAuction): void;
 }
 
 export interface IAuction {
     id: string;
     label: string;
-    numberOfBids: string;
-    highestBid: string;
+    numberOfBids: number;
+    minimunBidRequired?: number;
+    highestBid: number;
 }
